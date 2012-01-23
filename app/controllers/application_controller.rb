afcbdbf5ad6adbaa2405e4bc194fb6e6815619a8
polosilva <polo.silva.a@gmail.com>
2012-01-23 17:13:55 -0300

    Done with sign in & Chapter 9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  #Incluimos el modulo explicitamente pq a diferencia de las 
+  #vistas, en los controllers, no se agregan automaticamente
+  include SessionsHelper 
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 SampleApp::Application.routes.draw do
   
-  resources :users
+  get "sessions/new"
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   match '/signup',   :to => 'users#new'
-  
-  #match '/home',    :to => 'pages#home'
-  #match '/', :to => 'pages#home'
- 
+  match '/signin',   :to => 'sessions#new'
+  match '/signout',  :to => 'sessions#destroy'
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
