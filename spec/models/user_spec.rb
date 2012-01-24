dcca362098312b3481847dab253dbdcfb57296be
polosilva <polo.silva.a@gmail.com>
2012-01-24 20:04:41 -0300

    Added user microposts CH11

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,7 +50,7 @@ describe User do
     end
   end
 
-it "should reject invalid email addresses" do
+  it "should reject invalid email addresses" do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
       invalid_email_user = User.new(@attr.merge(:email => address))
@@ -58,7 +58,7 @@ it "should reject invalid email addresses" do
     end
   end
 
-it "should reject duplicate email addresses" do
+  it "should reject duplicate email addresses" do
     # Put a user with given email address into the database.
     User.create!(@attr)
     user_with_duplicate_email = User.new(@attr)
@@ -141,7 +141,7 @@ it "should reject duplicate email addresses" do
     end
   end
 
- describe "admin attribute" do
+  describe "admin attribute" do
 
     before(:each) do
       @user = User.create!(@attr)
@@ -160,4 +160,47 @@ it "should reject duplicate email addresses" do
       @user.should be_admin
     end
   end 
+
+  describe "micropost associations" do
+
+    before(:each) do
+      @user = User.create(@attr)
+      @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+      @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
+    end
+
+    it "should have a microposts attribute" do
+      @user.should respond_to(:microposts)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [@mp2, @mp1]
+    end
+
+    it "should destroy associated microposts" do
+      @user.destroy
+      [@mp1, @mp2].each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status feed" do
+
+      it "should have a feed" do
+        @user.should respond_to(:feed)
+      end
+
+      it "should include the user's microposts" do
+        @user.feed.include?(@mp1).should be_true
+        @user.feed.include?(@mp2).should be_true
+      end
+
+      it "should not include a different user's microposts" do
+        mp3 = Factory(:micropost,
+                      :user => Factory(:user, :email => Factory.next(:email)))
+        @user.feed.include?(mp3).should be_false
+      end
+    end
+  end
 end
+
